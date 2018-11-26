c491258bb83024d08103e1a32aa3aed057d20dd7
hrivierefr <legsmower@gmail.com>
2018-11-26 14:54:27 +0100

    Ajout des routes de base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :artisans
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	root to: 'item#index'
+  	devise_for :artisans
+  	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :carts
+	resources :orders
+	resources :items
 end
