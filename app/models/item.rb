4dfa9ca5720a48b39895d92179f84a3a22f51285
DamVan75 <damienvaneeckhout@gmail.com>
2018-11-26 15:34:02 +0100

    Validations items et artisans faites

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
 	has_many :cart_items
 	has_many :order_items
+	validates :title, :presence => true
+	validates :description, :presence => true
+	validates :image_url, :presence => true
+	validates :price, :presence => true
+	validates_format_of :price, :with => /^\d+\.*\d{0,2}$/
 end
