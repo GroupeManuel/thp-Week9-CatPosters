f7b89a89a67c910f527d1fe6c6b7e8278c61c03e
DamVan75 <damienvaneeckhout@gmail.com>
2018-11-28 10:34:46 +0100

    enlever la 2ème qui venait d'être ajouté dans le config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
-
 module ThpWeek9Catposters
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
