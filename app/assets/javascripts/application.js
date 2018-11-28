2a23c28187dc48afdec8165581aaf2dcf3d92346
wiliamailysh <wiliamailysh@gmail.com>
2018-11-28 14:01:13 +0100

    teeeest ma gueule

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,33 +16,3 @@
 //= require_tree .
 //= require turbolinks
 
-// Jquery and Bootstap core js files 
-//= require plugins/jquery.min
-//= require bootstrap/js/bootstrap.bundle.min
-
-// Isotope javascript
-//= require plugins/isotope/imagesloaded.pkgd.min
-//= require plugins/isotope/isotope.pkgd.min
-
-// Magnific Popup javascript
-//= require plugins/magnific-popup/jquery.magnific-popup.min
-
-// Appear javascript 
-//= require plugins/waypoints/jquery.waypoints.min
-//= require plugins/waypoints/sticky.min
-
-// Appear javascript
-//= require plugins/waypoints/jquery.waypoints.min
-//= require plugins/waypoints/sticky.min
-
-// Count To javascript 
-//= require plugins/countTo/jquery.countTo
-
-// Slick carousel javascript 
-//= require plugins/slick/slick.min
-
-// Initialization of Plugins
-//= require js/template
-
-// Custom Scripts
-//= require js/custom
